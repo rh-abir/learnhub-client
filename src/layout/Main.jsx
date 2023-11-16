291695a135959d64f474d3976747a834b323bb7e
rh-abir <rhabir.co@gmail.com>
2023-11-16 12:36:13 +0600

    setup react router

---
+++ src/layout/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+      <h2>THis is header</h2>
+      <Outlet />
+      <h2>This is footer</h2>
+    </div>
+  );
+};
+
+export default Main;
